a30a1f1fdf868640ca5f8f84dc105f3dc9016294
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-01-30 17:32:29 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thmenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Draw the triangle");
+                Console.WriteLine("2. Draw the square");
+                Console.WriteLine("3. Draw the rectangle");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Draw the triangle");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("Draw the square");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 3:
+                        Console.WriteLine("Draw the rectangle");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("No choice!");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
